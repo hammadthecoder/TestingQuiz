8edf5cb4a61cc4e1131f2c446f209d3df00a897c
PC <hammadbhat126@gmail.com>
2022-02-05 18:02:47 +0400

    Initial commit

---
+++ app/src/main/java/com/kashcode/testingquiz/activity/InstructionActivity.java
@@ -0,0 +1,49 @@
+
+package com.kashcode.testingquiz.activity;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.kashcode.testingquiz.R;
+
+public class InstructionActivity extends AppCompatActivity {
+
+    public Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_instruction);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getString(R.string.instruction));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public void Start(View view) {
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/kashcode/testingquiz/helper/VolleyCallback.java
@@ -0,0 +1,8 @@
+package com.kashcode.testingquiz.helper;
+
+public interface VolleyCallback {
+    void onSuccess(boolean result, String message);
+
+    //void onSuccessWithMsg(boolean result, String message);
+
+}
